ef24c3e8a7be4f817210054caf2b1e9031be79a6
shu Lin <bbqlinshu@gmail.com>
2022-03-11 22:25:19 -0500

    Completed assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,14 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+content = doc.css(".heading-financier").text.strip
+puts content
+courses = doc.css(".heading-35-semibold")
+courses.each do |course|
+    puts course.text.strip
+end
+
+p courses[0].name
+p courses[0].attributes
+p courses[0].children
